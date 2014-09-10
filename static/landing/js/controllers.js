1cd58b84411d1d220ff2bae180d12052de80e467
avara1986 <a.vara.1986@gmail.com>
2014-09-10 17:48:59 +0200

    Updated statics

--- static/landing/js/controllers.js
+++ static/landing/js/controllers.js
@@ -1,6 +1,9 @@
 angular.module('events', ['eventService'])
 .controller('eventController', ['$scope','$http', '$log', 'sharedEvent', function ($scope, $http, $log, sharedEvent) {
-	$scope.events = [];
+	/**
+	 * Inicializar events
+	 */
+	$scope.events = {};
 	$http.get('/api/events/').
     success(function(data, status, headers, config) {
         $scope.events = data;
@@ -20,23 +23,45 @@ angular.module('events', ['eventService'])
 
 angular.module('formAttendee', [])
 .controller('attendeeController', ['$scope','$http', '$log' , 'sharedEvent', function($scope, $http, $log, sharedEvent) {
-	$scope.form = {};
+
 	$scope.event = sharedEvent;
+	/**
+	 * Muestra si hubo un error en el envío(false) o la confirmación (true)
+	 */
 	$scope.result_register = false;
+	/**
+	 * Muestra el resultado del envío
+	 */
+	$scope.finish_sending = false;
+	/**
+	 * Muestra mensaje de "se está enviadno"
+	 */
 	$scope.is_sending_register = false;
-	$scope.attendee_id = "";
+	/**
+	 * Inicializar attendee
+	 */
+	$scope.attendee = {};
+	$scope.attendee.id = "";
+	/**
+	 * For tests
+	 */
+	$scope.attendee.name = "Alberto";
+	$scope.attendee.surname = "test2";
+	$scope.attendee.email = "test2@gmail.com";
+	$scope.attendee.web = "test2.com";
     $scope.submit = function(attendee,event) {
     	$scope.is_sending_register = true;
-        //var in_data = { subject: $scope.subject };
-    	attendee.event = event.id;
-    	//attendee.id = '1';
-    	//console.log(attendee);
-    	$http.post('api/attendees/', attendee)
+    	$scope.attendee = attendee;
+    	$scope.attendee.event = event.id;
+    	//attendee.event = event.id;
+    	$http.post('api/attendees/', $scope.attendee)
             .success(function(data) {
-            	$scope.is_sending_register = false;
             	console.log(data);
-            	$scope.attendee_id = data.id;
-            	$scope.result_register = true;
+            	$scope.is_sending_register = false;
+            	$scope.finish_sending = true;
+            	$scope.result_register = data.result;
+            	$scope.result_error_msg = data.error_msg;
+            	$scope.attendee = data;
             }).
             error(function(data, status, headers, config) {
                 console.log(data);
@@ -48,6 +73,4 @@ angular.module('formAttendee', [])
 	$scope.reset = function() {
 		$scope.attendee = angular.copy($scope.master);
 	};
-	
-	$scope.reset();
 }]);
