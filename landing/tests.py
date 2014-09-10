fbca33ff2e66ffafe63e7ce6b98cb418814b69ee
avara1986 <a.vara.1986@gmail.com>
2014-09-10 15:10:17 +0200

    Add Tox tests

--- landing/tests.py
+++ landing/tests.py
@@ -4,13 +4,16 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
+from events.api.api import EventList, AttendeeList
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class landingTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_200(self):
+        index_res = self.client.get(reverse('index_landing'))
+        self.assertEqual(index_res.status_code, 200)
